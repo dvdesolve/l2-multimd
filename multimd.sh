0ba409bf36eababc36d9a81ec279495dd5c16379
Drobot Viktor <linux776@gmail.com>
2018-11-23 14:39:17 +0300

    Added support for NAMD engine in multimd.sh

--- multimd.sh
+++ multimd.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/bash
 
-# error codes
+
+### error codes
 E_NOTABASH=1
 E_OLD_BASH=2
-E_FEW_ARGS=3
-E_INV_CONF=4
-E_INV_TASK=5
-E_NO_SLURM=6
-E_RUN_FAIL=7
+E_POS_ARGS=3
+E_UNK_ENGN=4
+E_INV_CONF=5
+E_INV_TASK=6
+E_NO_SLURM=7
+E_RUN_FAIL=8
 
 
-# coloring support
+### supported engines
+ENG_AMBER=1
+ENG_NAMD=2
+
+
+### coloring support
 C_RED='\033[1;31m'
 C_GREEN='\033[1;32m'
 C_BLUE='\033[1;34m'
@@ -22,7 +29,7 @@ C_NC='\033[0m'
 # print header
 echo -e "${C_BLUE}+-----------------------------------------------+${C_NC}"
 echo -e "${C_BLUE}|                                               |${C_NC}"
-echo -e "${C_BLUE}| ${C_YELLOW}Lomonosov-2 batch wrapper v0.1.0 (22.11.2018) ${C_BLUE}|${C_NC}"
+echo -e "${C_BLUE}| ${C_YELLOW}Lomonosov-2 batch wrapper v0.2.0 (22.11.2018) ${C_BLUE}|${C_NC}"
 echo -e "${C_BLUE}|           ${C_YELLOW}Written by Viktor Drobot            ${C_BLUE}|${C_NC}"
 echo -e "${C_BLUE}|                                               |${C_NC}"
 echo -e "${C_BLUE}+-----------------------------------------------+${C_NC}"
@@ -30,7 +37,7 @@ echo
 echo
 
 
-# some checks
+# perform some checks
 if [ -z "$BASH_VERSION" ]
 then
     echo -e "${C_RED}ERROR: this script support only BASH interpreter! Exiting.${C_NC}" >&2
@@ -43,31 +50,39 @@ then
     exit $E_OLD_BASH
 fi
 
-if ! command -v sbatch > /dev/null 2>&1
+# TEMP for testing only!
+#if ! command -v sbatch > /dev/null 2>&1
+#then
+#    echo -e "${C_RED}ERROR: no SLURM tools are found (maybe you forgot about 'module load'?)! Exiting.${C_NC}" >&2
+#    exit $E_NO_SLURM;
+#fi
+
+# usage help
+if [[ "$#" -ne 2 ]]
 then
-    echo -e "${C_RED}ERROR: no SLURM tools are found (maybe you forgot about 'module load'?)! Exiting.${C_NC}" >&2
-    exit $E_NO_SLURM;
+    echo "Usage: $0 engine taskfile"
+    exit $E_POS_ARGS
 fi
 
-if [[ "$#" -ne 1 ]]
-then
-    echo "Usage: $0 taskfile"
-    exit $E_FEW_ARGS
-fi
+
+### list of known keywords
+KEYWORDS="DATAROOT AMBERROOT NAMDROOT RUNTIME PARTITION NUMNODES BIN TASK"
 
 
-# list of known keywords
-KEYWORDS="DATAROOT AMBERROOT RUNTIME PARTITION NUMNODES BIN TASK"
-
-
-# some defaults
+### some defaults
 SLEEPTIME=5
+
+declare -i ENGINE
+ENGINE=$ENG_AMBER
+
 AMBERTASK="${HOME}/_scratch/opt/l2-multimd/amber-wrapper.sh"
+NAMDTASK="${HOME}/_scratch/opt/l2-multimd/namd-wrapper.sh"
 
 
-# default settings for executing tasks
+### default settings for executing tasks
 DATAROOT=''
 AMBERROOT=''
+NAMDROOT=''
 RUNTIME='1-00:00:00'
 PARTITION='compute'
 
@@ -80,26 +95,26 @@ declare -i NUMTASKS
 NUMTASKS=0
 
 
-# here we will store our configurations
+### here we will store our configurations
 declare -a T_DIRS
 declare -a T_NODES
 declare -a T_BINS
-declare -a T_PRMTOPS
 declare -a T_CONFIGS
-declare -a T_COORDS
 declare -a T_OUTPUTS
-declare -a T_RESTARTS
-declare -a T_TRAJS
-declare -a T_INFOS
+declare -a T_AMB_PRMTOPS
+declare -a T_AMB_COORDS
+declare -a T_AMB_RESTARTS
+declare -a T_AMB_TRAJS
+declare -a T_AMB_INFOS
 
 
-# remove preceding spaces from the string
+### remove preceding spaces from the string
 chomp () {
     echo "$1" | sed -e 's/^[ \t]*//'
 }
 
 
-# parse TASK keyword
+### parse TASK keyword
 task () {
     declare -i idx
     idx="$1"
@@ -117,13 +132,29 @@ task () {
     # apply default parameters from config file
     T_NODES[$idx]="$NUMNODES"
     T_BINS[$idx]="$BIN"
-    T_PRMTOPS[$idx]="${T_DIRS[$idx]}.prmtop"
-    T_CONFIGS[$idx]="${T_DIRS[$idx]}.in"
-    T_COORDS[$idx]="${T_DIRS[$idx]}.rst"
+
+    EXT=''
+
+    if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+    then
+        EXT='in'
+    elif [[ "$ENGINE" -eq "$ENG_NAMD" ]]
+    then
+        EXT='conf'
+    fi
+
+    T_CONFIGS[$idx]="${T_DIRS[$idx]}.${EXT}"
+
     T_OUTPUTS[$idx]="${T_DIRS[$idx]}.out"
-    T_RESTARTS[$idx]="${T_DIRS[$idx]}.rst"
-    T_TRAJS[$idx]="${T_DIRS[$idx]}.nc"
-    T_INFOS[$idx]="${T_DIRS[$idx]}.mdinfo"
+
+    if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+    then
+        T_AMB_PRMTOPS[$idx]="${T_DIRS[$idx]}.prmtop"
+        T_AMB_COORDS[$idx]="${T_DIRS[$idx]}.rst"
+        T_AMB_RESTARTS[$idx]="${T_DIRS[$idx]}.rst"
+        T_AMB_TRAJS[$idx]="${T_DIRS[$idx]}.nc"
+        T_AMB_INFOS[$idx]="${T_DIRS[$idx]}.mdinfo"
+    fi
 
     # parse remaining positional parameters
     while [[ $# -gt 0 ]]
@@ -153,17 +184,6 @@ task () {
                 shift 2
                 ;;
 
-            -p|--prmtop)
-                if [[ "$#" -lt 2 ]]
-                then
-                    echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
-                    exit $E_INV_TASK
-                fi
-
-                T_PRMTOPS[$idx]="$2"
-                shift 2
-                ;;
-
             -i|--config)
                 if [[ "$#" -lt 2 ]]
                 then
@@ -172,17 +192,6 @@ task () {
                 fi
 
                 T_CONFIGS[$idx]="$2"
-                shift 2
-                ;;
-
-            -c|--coord)
-                if [[ "$#" -lt 2 ]]
-                then
-                    echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
-                    exit $E_INV_TASK
-                fi
-
-                T_COORDS[$idx]="$2"
                 shift 2
                 ;;
 
@@ -197,36 +206,88 @@ task () {
                 shift 2
                 ;;
 
-            -r|--restart)
-                if [[ "$#" -lt 2 ]]
+            -p|--prmtop)
+                if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
                 then
-                    echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
-                    exit $E_INV_TASK
+                    if [[ "$#" -lt 2 ]]
+                    then
+                        echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
+                        exit $E_INV_TASK
+                    fi
+
+                    T_AMB_PRMTOPS[$idx]="$2"
+                else
+                    echo -e "${C_RED}WARNING: skipping AMBER-related parameter [$token] in task definition #$((idx + 1)) (line #$lineno)...${C_NC}" >&2
                 fi
 
-                T_RESTARTS[$idx]="$2"
+                shift 2
+                ;;
+
+            -c|--coord)
+                if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+                then
+                    if [[ "$#" -lt 2 ]]
+                    then
+                        echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
+                        exit $E_INV_TASK
+                    fi
+
+                    T_AMB_COORDS[$idx]="$2"
+                else
+                    echo -e "${C_RED}WARNING: skipping AMBER-related parameter [$token] in task definition #$((idx + 1)) (line #$lineno)...${C_NC}" >&2
+                fi
+
+                shift 2
+                ;;
+
+            -r|--restart)
+                if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+                then
+                    if [[ "$#" -lt 2 ]]
+                    then
+                        echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
+                        exit $E_INV_TASK
+                    fi
+
+                    T_AMB_RESTARTS[$idx]="$2"
+                else
+                    echo -e "${C_RED}WARNING: skipping AMBER-related parameter [$token] in task definition #$((idx + 1)) (line #$lineno)...${C_NC}" >&2
+                fi
+
                 shift 2
                 ;;
 
             -x|--traj)
-                if [[ "$#" -lt 2 ]]
+                if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
                 then
-                    echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
-                    exit $E_INV_TASK
+                    if [[ "$#" -lt 2 ]]
+                    then
+                        echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
+                        exit $E_INV_TASK
+                    fi
+
+                    T_AMB_TRAJS[$idx]="$2"
+                else
+                    echo -e "${C_RED}WARNING: skipping AMBER-related parameter [$token] in task definition #$((idx + 1)) (line #$lineno)...${C_NC}" >&2
                 fi
 
-                T_TRAJS[$idx]="$2"
                 shift 2
                 ;;
 
             -inf|--mdinfo)
-                if [[ "$#" -lt 2 ]]
+                if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
                 then
-                    echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
-                    exit $E_INV_TASK
+                    if [[ "$#" -lt 2 ]]
+                    then
+                        echo -e "${C_RED}ERROR: something wrong with the task definition #$((idx + 1)) (line #$lineno)!${C_NC}" >&2
+                        exit $E_INV_TASK
+                    fi
+
+                    T_AMB_INFOS[$idx]="$2"
+                else
+                    echo -e "${C_RED}WARNING: skipping AMBER-related parameter [$token] in task definition #$((idx + 1)) (line #$lineno)...${C_NC}" >&2
                 fi
 
-                T_INFOS[$idx]="$2"
                 shift 2
                 ;;
 
@@ -239,7 +300,27 @@ task () {
 }
 
 
-# main script starts here
+### main script starts here
+
+
+# determine which engine should we use
+case "${1^^}" in
+    "AMBER")
+        ENGINE=$ENG_AMBER
+        ;;
+
+    "NAMD")
+        ENGINE=$ENG_NAMD
+        ;;
+
+    *)
+        echo -e "${C_RED}ERROR: unsupported engine [$1] is given! Exiting.${C_NC}" >&2
+        exit $E_UNK_ENGN
+        ;;
+esac
+
+# drop engine argument
+shift
 
 
 # some helpful variables
@@ -275,13 +356,27 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     # extract remaining parameters and store needed data
     PARAMS=$(chomp "`echo "$line" | awk '{$1 = ""; print $0}'`")
 
-    case "${KEYWORD^^}" in # TODO parse keywords with one spaces-free parameter in robust way (such as awk '{print $1}')
+    case "${KEYWORD^^}" in # TODO parse keywords with one spaces-free parameter in more robust way (such as awk '{print $1}')
         "DATAROOT")
             DATAROOT="$PARAMS"
             ;;
 
         "AMBERROOT")
-            AMBERROOT="$PARAMS"
+            if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+            then
+                AMBERROOT="$PARAMS"
+            else
+                echo -e "${C_RED}WARNING: ignoring AMBER-related keyword [$KEYWORD] (line #$lineno)...${C_NC}" >&2
+            fi
+            ;;
+
+        "NAMDROOT")
+            if [[ "$ENGINE" -eq "$ENG_NAMD" ]]
+            then
+                NAMDROOT="$PARAMS"
+            else
+                echo -e "${C_RED}WARNING: ignoring NAMD-related keyword [$KEYWORD] (line #$lineno)...${C_NC}" >&2
+            fi
             ;;
 
         "RUNTIME") # TODO see above
@@ -313,15 +408,15 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 done < "$1"
 
 
-# total number of task to run
+# total number of tasks to run
 declare -i NUMTASKS
 NUMTASKS="$task_idx"
 
 
 # check if something wrong with given taskfile, e. g. necessary keywords are omitted or no tasks to run
-if [[ -z "$DATAROOT" || -z "$AMBERROOT" || -z "$RUNTIME" || -z "$PARTITION" || "$NUMTASKS" -eq 0 ]]
+if [[ -z "$DATAROOT" || -z "$AMBERROOT$NAMDROOT" || -z "$RUNTIME" || -z "$PARTITION" || "$NUMTASKS" -eq 0 ]]
 then
-    echo -e "${C_RED}ERROR: something wrong with taskfile (check DATAROOT, AMBERROOT, RUNTIME, PARTITION directives and the number of tasks given)!${C_NC}" >&2
+    echo -e "${C_RED}ERROR: something wrong with taskfile (check DATAROOT, AMBERROOT/NAMDROOT, RUNTIME, PARTITION directives and the number of tasks given)!${C_NC}" >&2
     exit $E_INV_CONF
 fi
 
@@ -331,7 +426,15 @@ echo -e "${C_BLUE}===========${C_NC}"
 echo -e "${C_BLUE}JOB SUMMARY${C_NC}"
 echo -e "${C_BLUE}===========${C_NC}"
 echo -e "Base data directory is ${C_YELLOW}[$DATAROOT]${C_NC}"
-echo -e "AMBER is installed into ${C_YELLOW}[$AMBERROOT]${C_NC}"
+
+if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+then
+    echo -e "Will use AMBER engine. AMBER is installed into ${C_YELLOW}[$AMBERROOT]${C_NC}"
+elif [[ "$ENGINE" -eq "$ENG_NAMD" ]]
+then
+    echo -e "Will use NAMD engine. NAMD is installed into ${C_YELLOW}[$NAMDROOT]${C_NC}"
+fi
+
 echo -e "Time limit for the whole job is ${C_YELLOW}[$RUNTIME]${C_NC}"
 echo -e "We will use ${C_YELLOW}[$PARTITION]${C_NC} partition to run our tasks"
 echo -e "One task will consume ${C_YELLOW}[$NUMNODES]${C_NC} nodes by default"
@@ -364,24 +467,34 @@ do
     echo -e "Data directory is ${C_YELLOW}[${T_DIRS[$task_idx]}]${C_NC}"
     echo -e "Will use ${C_YELLOW}[${T_NODES[$task_idx]}]${C_NC} nodes"
     echo -e "Executable binary is ${C_YELLOW}[${T_BINS[$task_idx]}]${C_NC}"
-    echo -e "Topology file is ${C_YELLOW}[${T_PRMTOPS[$task_idx]}]${C_NC}"
     echo -e "Config file is ${C_YELLOW}[${T_CONFIGS[$task_idx]}]${C_NC}"
-    echo -e "Start coordinates are in file ${C_YELLOW}[${T_COORDS[$task_idx]}]${C_NC}"
     echo -e "Output file is ${C_YELLOW}[${T_OUTPUTS[$task_idx]}]${C_NC}"
-    echo -e "Restart will be written to file ${C_YELLOW}[${T_RESTARTS[$task_idx]}]${C_NC}"
-    echo -e "Trajectories will be written to file ${C_YELLOW}[${T_TRAJS[$task_idx]}]${C_NC}"
-    echo -e "MD information will be available in file ${C_YELLOW}[${T_INFOS[$task_idx]}]${C_NC}"
 
-    if [[ "${T_COORDS[$task_idx]}" == "${T_RESTARTS[$task_idx]}" ]]
+    if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
     then
-        echo -e "${C_RED}WARNING: coordinates and restart files are the same! Original coordinates will be overwritten!${C_NC}" >&2
+        echo -e "Topology file is ${C_YELLOW}[${T_AMB_PRMTOPS[$task_idx]}]${C_NC}"
+        echo -e "Start coordinates are in file ${C_YELLOW}[${T_AMB_COORDS[$task_idx]}]${C_NC}"
+        echo -e "Restart will be written to file ${C_YELLOW}[${T_AMB_RESTARTS[$task_idx]}]${C_NC}"
+        echo -e "Trajectories will be written to file ${C_YELLOW}[${T_AMB_TRAJS[$task_idx]}]${C_NC}"
+        echo -e "MD information will be available in file ${C_YELLOW}[${T_AMB_INFOS[$task_idx]}]${C_NC}"
+
+        if [[ "${T_AMB_COORDS[$task_idx]}" == "${T_AMB_RESTARTS[$task_idx]}" ]]
+        then
+            echo -e "${C_RED}WARNING: coordinates and restart files are the same! Original coordinates will be overwritten!${C_NC}" >&2
+        fi
     fi
 
     echo -e "${C_BLUE}------${C_NC}"
     echo -n -e "Trying to save prepared command to ${C_YELLOW}[${DATAROOT%/}/${T_DIRS[$task_idx]}/runcmd.$$]${C_NC}... "
 
     # now we'll build final execution line...
-    COMMAND="\"$AMBERROOT/bin/${T_BINS[$task_idx]}\" -O -i \"${T_CONFIGS[$task_idx]}\" -o \"${T_OUTPUTS[$task_idx]}\" -p \"${T_PRMTOPS[$task_idx]}\" -c \"${T_COORDS[$task_idx]}\" -r \"${T_RESTARTS[$task_idx]}\" -x \"${T_TRAJS[$task_idx]}\" -inf \"${T_INFOS[$task_idx]}\""
+    if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+    then
+        COMMAND="\"$AMBERROOT/bin/${T_BINS[$task_idx]}\" -O -i \"${T_CONFIGS[$task_idx]}\" -o \"${T_OUTPUTS[$task_idx]}\" -p \"${T_AMB_PRMTOPS[$task_idx]}\" -c \"${T_AMB_COORDS[$task_idx]}\" -r \"${T_AMB_RESTARTS[$task_idx]}\" -x \"${T_AMB_TRAJS[$task_idx]}\" -inf \"${T_AMB_INFOS[$task_idx]}\""
+    elif [[ "ENGINE" -eq "$ENG_NAMD" ]]
+    then
+        COMMAND="\"${T_CONFIGS[$task_idx]}\" \"$NAMDROOT/${T_BINS[$task_idx]}\" +isomalloc_sync +idlepoll"
+    fi
 
     # ...and store it in appropriate place
     echo "$COMMAND" 2> /dev/null > "${DATAROOT%/}/${T_DIRS[$task_idx]}/runcmd.$$"
@@ -411,24 +524,35 @@ echo
 
 
 # prepare SLURM command
-CMD="sbatch -N $TOTALNODES -p $PARTITION -t $RUNTIME $AMBERTASK $$ $RUNTIME $DATAROOT"
+WRAPPER=''
+
+if [[ "$ENGINE" -eq "$ENG_AMBER" ]]
+then
+    WRAPPER="$AMBERTASK"
+elif [[ "$ENGINE" -eq "$ENG_NAMD" ]]
+then
+    WRAPPER="$NAMDTASK"
+fi
+
+CMD="sbatch -N $TOTALNODES -p $PARTITION -t $RUNTIME $WRAPPER $$ $RUNTIME $DATAROOT"
 echo "Command that will be run:"
 echo -e "${C_YELLOW}$CMD${C_NC}"
 echo
 echo
 
+# TEMP for testing only!
 # go to the scratch root and submit job
-cd "${HOME}/_scratch"
+#cd "${HOME}/_scratch"
 
-TASKID=`$CMD | grep 'Submitted batch job' | awk '{print $NF}'`
+#TASKID=`$CMD | grep 'Submitted batch job' | awk '{print $NF}'`
 
-if [[ "$?" -eq 0 ]]
-then
-    echo -e "Job submitted successfully. SLURM job ID is ${C_YELLOW}[$TASKID]${C_NC}"
-else
-    echo -e "${C_RED}ERROR: something wrong with job queueing! Check SLURM output.${C_NC}" >&2
-    exit $E_RUN_FAIL
-fi
+#if [[ "$?" -eq 0 ]]
+#then
+#    echo -e "Job submitted successfully. SLURM job ID is ${C_YELLOW}[$TASKID]${C_NC}"
+#else
+#    echo -e "${C_RED}ERROR: something wrong with job queueing! Check SLURM output.${C_NC}" >&2
+#    exit $E_RUN_FAIL
+#fi
 
 
 # we're done here
